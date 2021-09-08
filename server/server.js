e3466f91a46e9e754d1fa448183034d72e77cdea
navigsb <navigsb@uw.edu>
2021-09-07 17:49:26 -0700

    Switch to React

--- server/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-let server = app.listen(3000, function() {
-	console.log("Listening on port 3000...");
-});
-
-app.use(express.static("../code"));
