6bf4c5df95016922e4246af299a32d2256ccd37d
navigsb <navigsb@uw.edu>
2021-09-07 18:20:33 -0700

    Setup Tailwind in React

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-green-600">
+      <p className="py-1 px-2">Testing Tailwind!!</p>
+    </div>
+  );
+}
+
+export default App;
